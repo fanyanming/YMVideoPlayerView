797010037da8624cd1fe2110997d5e6f9dbb91ae
Yanming <developerfan@siunion.com.cn>
2017-08-15 11:40:21 +0800

    Demo添加

---
+++ YMVideoPlayerView/YMVideoPlayerView/PlayerView/YMVideoPlayerView.h
@@ -0,0 +1,16 @@
+//
+//  YMVideoPlayerView.h
+//  CrazyBeeFitness
+//
+//  Created by 彦明 on 2017/7/19.
+//  Copyright © 2017年 siunion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMVideoPlayerView : UIView
+//@property (nonatomic, copy) NSString *videoUrlString;
+//@property (nonatomic, copy) NSString *videoCoverUrlString;
+/** 默认和建议的播放器比例为16:9 */
+- (instancetype)initWithFrame:(CGRect)frame UrlString:(NSString *)urlString;
+@end
---
+++ YMVideoPlayerView/YMVideoPlayerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMVideoPlayerView
+//
+//  Created by 樊彦明 on 2017/8/15.
+//  Copyright © 2017年 Yanming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
